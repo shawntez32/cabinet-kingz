af929625f7adbecc59153dea5111d8c7f1df549e
Shawntez Malone <69469670+shawntez32@users.noreply.github.com>
2022-05-19 14:06:48 -0400

    Update cbkBackend.py

--- cbkBackend.py
+++ cbkBackend.py
@@ -1,33 +1,34 @@
 import datetime as dt
 import sqlite3 as sql
 
+measures = ['per-gram','per-ounce','per-pound','per-gallon','per-case','per-bag']
+items = []
+
+def dbItems():
+    conn = sql.connect('dbs/grocerylist.db')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM Items')
+    rows = cur.fetchall()
+    conn.close()
+    for item in rows:
+        items.append(item)
 
 class FoodItem:
-    measures = ['per-gram','per-ounce','per-pound','per-gallon','per-case','per-bag']
 
-    def __init__(self,name,category,measure,id,price):
+    def __init__(self,name,category,measure,price):
         self.name = name
         self.category = category
         self.measure = measure
         self.id = id
         self.price = price
 
-    def addItem(self):
-        conn = sql.connect('grocerylist.db')
+    def add_item(self):
+        conn = sql.connect('dbs/grocerylist.db')
         cur = conn.cursor()
-        cur.execute('CREATE TABLE IF NOT EXISTS Items(Name TEXT,Category TEXT,Measure TEXT,ID INT,Price INT)')
-        cur.execute('INSERT INTO Items(Name,Category,Measure,ID,Price) VALUES(?,?,?,?,?)',(self.name,self.category,self.measure,self.id,self.price))
+        cur.execute('CREATE TABLE IF NOT EXISTS Items(Name TEXT,Category TEXT,Measure TEXT,id AUTO INTERGER PRIMARY KEY,Price INT)')
+        cur.execute('INSERT INTO Items(Name,Category,Measure,Price) VALUES(?,?,?,?)',(self.name,self.category,self.measure,self.price))
         conn.commit()
         conn.close()
-
-    def dbItems(self):
-        conn = sql.connect('grocerylist.db')
-        cur = conn.cursor()
-        cur.execute('SELECT * FROM Items')
-        items = cur.fetchall()
-        conn.close()
-        for item in items:
-            x = FoodItem(item[0],item[1],item[2],item[3],item[4])
 
 class Expense:
     occurences = ['once','daily','weekly','monthly','custom']
@@ -45,7 +46,7 @@ class Expense:
         conn.commit()
 
 class CurInventory:
-    def __init__(self,name,category,measure,id,price,inStock,suff,succ):
+    def __init__(self,name,category,measure,id,price,inStock,suff,succ,stkFor):
         self.name = name
         self.category = category
         self.measure = measure
@@ -54,6 +55,7 @@ class CurInventory:
         self.stock = inStock
         self.suff = suff
         self.succ = succ
+        self.stkFor = stkFor
 
 class Recipe:
     def __init__(self,name,recipeDict):
@@ -61,20 +63,35 @@ class Recipe:
         self.recipe = recipeDict
 
 class Paycheck:
-    def __init__(self,date,source,amt,taxes):
+    def __init__(self,date,source,amt,taxes,id):
         self.date = date
         self.source = source
         self.amt = amt
         self.taxes = taxes
 
 class Budget:
-    def __init__(self,income,budgetDict,timeframe):
+    def __init__(self,income,budgetDict,timeframe,id):
         self.income = income
         self.budget = budgetDict
         self.tf = timeframe
 
 class Goal:
-    def __init__(self,price,importance,goalDate):
+    def __init__(self,price,importance,goalDate,id):
         self.price = price
         self.imp = importance
         self.gDate = goalDate
+
+#measures = ['per-gram','per-ounce','per-pound','per-gallon','per-case','per-bag']
+
+names = []
+category = []
+price = []
+
+dbItems()
+
+for e in items:
+    names.append(e[0])
+    category.append(e[1])
+    price.append(e[4])
+
+
